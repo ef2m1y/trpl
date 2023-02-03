55aa0c1b4159d421756691d66cdc67a24d5a5199
ef2m1y <shisotem01072002@gmail.com>
2023-02-03 23:14:42 +0900

    12.1

---
+++ minigrep/target/debug/deps/minigrep-a96225767c2cdeba.d
@@ -0,0 +1,5 @@
+/home/ilu/trpl/minigrep/target/debug/deps/minigrep-a96225767c2cdeba: src/main.rs
+
+/home/ilu/trpl/minigrep/target/debug/deps/minigrep-a96225767c2cdeba.d: src/main.rs
+
+src/main.rs:
---
+++ minigrep/target/debug/minigrep.d
@@ -0,0 +1,1 @@
+/home/ilu/trpl/minigrep/target/debug/minigrep: /home/ilu/trpl/minigrep/src/main.rs
