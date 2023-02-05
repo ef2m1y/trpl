de9b393582f384c1477d408682348a6e35ec5a8d
ef2m1y <shisotem01072002@gmail.com>
2023-02-05 23:31:28 +0900

    12.4

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -25,5 +25,25 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let mut contents = String::new();
     f.read_to_string(&mut contents)?;
 
+    // println!("With text:\n{}", contents);
     Ok(())
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(
+            vec!["safe, fast, productive."],
+            search(query, contents)
+        );
+    }
 }
